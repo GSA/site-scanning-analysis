a468a365f61f459b3e543ec3bfbc16a707f226d9
Andy Kuny <andrew.kuny@gsa.gov>
2023-06-23 12:06:17 -0400

    Update config

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ config = {
     'all_snapshot_report_location': os.path.join(dirname, './reports/snapshot-all.csv'),
     'unique_url_report_location': os.path.join(dirname, './reports/unique-url.csv'),
     'unique_website_report_location': os.path.join(dirname, './reports/unique-website.csv'),
-    'target_url_list_report_location': os.path.join(dirname, './reports/target-URL-list.csv'),
+    'target_url_list_report_location': os.path.join(dirname, './reports/target-url-list.csv'),
     # unique url and website file locations
     'initial_dataset_location': os.path.join(dirname, './unique_website_list/results/initial_dataset.csv'),
     'unique_final_urls_location': os.path.join(dirname, './unique_website_list/results/weekly-snapshot-unique-final-urls.csv'),
