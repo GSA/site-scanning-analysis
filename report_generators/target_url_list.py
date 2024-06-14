f61cc13006b91806d6298ca90405123b9f4f7b1b
Andy Kuny <andrew.kuny@gsa.gov>
2024-06-14 08:29:37 -0400

    Fix target url report

--- report_generators/target_url_list.py
+++ report_generators/target_url_list.py
@@ -10,14 +10,12 @@ class TargetUrlList:
             'How many target urls are in the list?': self.num_records(),
             'How many unique base domains are in the list?': self.num_unique('base_domain'),
             'How many unique agencies are in the list?': self.num_unique('agency'),
-            'How many unique agency codes are in the list?': self.num_unique('agency_code'),
             'How many unique bureaus are in the list?': self.num_unique('bureau'),
-            'How many unique bureau codes are in the list?': self.num_unique('bureau_code'),
             'How many urls are sourced from the list of federal domains?': self.num_true('source_list_federal_domains'),
             'How many urls are sourced from pulse?': self.num_true('source_list_pulse'),
             'How many urls are sourced from DAP?': self.num_true('source_list_dap'),
             'How many urls are sourced from the other websites list?': self.num_true('source_list_other'),
-            'How many blank cells are there in the target URL list (excluding the agency code, bureau, and bureau code columns)?': self.num_blank(),
+            'How many blank cells are there in the target URL list?': self.num_blank(),
         }
 
     def num_records(self):
@@ -36,5 +34,4 @@ class TargetUrlList:
         return len(self.df.loc[self.df[field].notna()])
 
     def num_blank(self):
-        temp_df = self.df.drop(columns=['agency_code', 'bureau_code'])
-        return temp_df.isna().sum().sum()
+        return self.df.isna().sum().sum()
