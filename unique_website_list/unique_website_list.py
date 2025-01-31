7a556ec11d69048f810f50ed802dac8df997c40f
Luke Chavers <lucas.chavers@gsa.gov>
2025-01-31 16:23:21 -0500

    Update unique website list for new field names

--- unique_website_list/unique_website_list.py
+++ unique_website_list/unique_website_list.py
@@ -8,21 +8,21 @@ def generate_unique_website_list():
     df.to_csv(config['initial_dataset_location'], index=False)
 
     # Drop rows with duplicate final_url values and save
-    removed_df = df.loc[df.duplicated('final_url')]
+    removed_df = df.loc[df.duplicated('url')]
     removed_df.to_csv(config['removed_final_urls_location'], index=False)
-    df = df.drop_duplicates('final_url', keep='first')
+    df = df.drop_duplicates('url', keep='first')
     df.to_csv(config['unique_final_urls_location'], index=False)
 
     # Reindex by length of final_url
-    s = df.final_url.str.len().sort_values().index
+    s = df.url.str.len().sort_values().index
     df = df.reindex(s)
 
     # Drop rows with duplicate final_url_website values and save
-    blank_final_url_website_df = df[df['final_url_website'].isna()]
-    df = df[~df['final_url_website'].isna()]
-    removed_df = df.loc[df.duplicated('final_url_website')]
+    blank_final_url_website_df = df[df['top_level_domain'].isna()]
+    df = df[~df['top_level_domain'].isna()]
+    removed_df = df.loc[df.duplicated('top_level_domain')]
     removed_df.to_csv(config['removed_final_url_websites_location'], index=False)
-    df = df.drop_duplicates('final_url_website', keep='first')
-    df = df.sort_values(by='final_url_website')
+    df = df.drop_duplicates('top_level_domain', keep='first')
+    df = df.sort_values(by='top_level_domain')
     final_df = pd.concat([df, blank_final_url_website_df])
     final_df.to_csv(config['unique_final_websites_location'], index=False)
