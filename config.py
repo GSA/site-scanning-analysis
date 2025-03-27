eba17f4c5823719ffb0ccab384c7125031120442
Luke Chavers <lucas.chavers@gsa.gov>
2025-03-26 20:14:42 -0400

    Create Federal standards snapshot

--- config.py
+++ config.py
@@ -32,5 +32,7 @@ config = {
     # base consumer report
     'base_consumer_report_location': os.path.join(dirname, './snapshots/base_consumer.csv'),
     # missing target urls in snapshot report
-    'missing_target_url_report_location': os.path.join(dirname, './reports/missing-target-url-in-snapshot.csv')
+    'missing_target_url_report_location': os.path.join(dirname, './reports/missing-target-url-in-snapshot.csv'),
+    # federal standards snapshot
+    'federal_standards_snapshot_url': os.path.join(dirname, './snapshots/standards.csv')
 }
